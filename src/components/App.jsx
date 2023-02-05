8abdf5d06e3b045fa5e1f9f794787c8536c494d5
Kostiantyn Harbuz <konst.garbuz@gmail.com>
2023-02-05 18:08:51 +0200

    додає виконання всього завдання

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,86 @@
 import React, { Component } from "react";
+import { ContactForm } from "./ContactForm/ContactForm";
+import { ContactList } from "./ContactList/ContactList";
+import { Filter } from "./Filter/Filter";
+import { nanoid } from "nanoid";
 
 export class App extends Component {
 
   state = {
-  contacts: [],
-  name: ''
-}
+    contacts: [  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+
+    filter: ''
+  }
+
+  addContact = ({ name, number }) => {
+      const { contacts } = this.state;
+      if (this.isDublcate(name)) {
+        return alert(`${name} is already in contacts.`)
+      }
+    this.setState((prevState) => {
+      const newContact = {
+        id: nanoid(),
+        name,
+        number
+      }
+      return {contacts: [newContact, ...contacts], name:'', number:''}
+    })
+  }
+
+    isDublcate(name) {
+      const normolizeName = name.toLocaleLowerCase();
+      const { contacts } = this.state;
+      const contact = contacts.find(({ name }) => {
+        return(name.toLocaleLowerCase() === normolizeName)
+      });
+      return Boolean(contact);
+    }
+
+  removeContact = (id) => {
+    this.setState(({contacts}) => {
+      const newContacts = contacts.filter(contact => contact.id !== id)
+      return {contacts: newContacts}  
+    })
+  }
+
+  handlChangeFilter = ({ target }) => {
+    this.setState(
+     { filter: target.value}
+    )
+  }
+
+  getFilteredContacts() {
+    const { filter, contacts } = this.state;
+    if (!filter) {
+      return contacts;
+    }
+    const normalazeFilter = filter.toLocaleLowerCase();
+    const result = contacts.filter(({ name }) => {
+      return(name.toLocaleLowerCase().includes(normalazeFilter))
+    })
+
+    return result;
+  }
 
   render() {
 
+    const contacts = this.getFilteredContacts();
+    
     return (
       <>
         <div>
-          <h2>Phonebook</h2>
-        <form>
-          <label>Name:
-        <input
-  type="text"
-  name="name"
-  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-  required
-          />
-          <button type="submit">Add contact</button>
-          </label>
-          </form>
-        </div>
-        <div>
+          <h1>Phonebook</h1>
+          <ContactForm onSubmit={this.addContact} />
+
           <h2>Contacts</h2>
-          <ul>
-            <li>Jacob Mercer</li>
-          </ul>
+          <Filter handlChange={this.handlChangeFilter} />
+          <ContactList contacts={contacts} removeContact={this.removeContact}/>
         </div>
       </>
     )
     
   }
-
 };
